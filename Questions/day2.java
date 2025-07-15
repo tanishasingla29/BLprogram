6508a22b68dceb4bdbe1357fc5e146e9e2d08e4e
tanisha <tanisha-email@example.com>
2025-07-15 10:29:38 +0530

    day 2 programs

---
+++ Questions/day2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class day2 {
+    
+    public static void main(String[] args){
+        Scanner abc = new Scanner(System.in);
+        System.out.println("Enter you Name: ");
+        String name = abc.nextLine();
+         System.out.println(" Name: "+name);
+         System.out.println("Enter you College Name: ");
+         String collegeName = abc.nextLine();
+         
+         System.out.println(" College Name: "+collegeName);
+         System.out.println("Enter you roll_no: ");
+         int roll_no = abc.nextInt();
+          System.out.println(" Roll_no. : "+roll_no);
+          System.out.println(" Age: ");
+          int age = abc.nextInt();
+          System.out.println(" Age : "+age);
+    }
+}
